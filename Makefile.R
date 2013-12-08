d61311d7a9bc54d1b8c47bb41fb9e3e57bad3b2d
jennybc <jenny@stat.ubc.ca>
2013-12-07 16:06:23 -0800

    More changes related to reorganization; figures output by first script

--- Makefile.R
+++ Makefile.R
@@ -2,10 +2,9 @@
 
 ## clean out any previous work
 outputs <- c("data/abalone_clean.csv",
-             list.files("Graphs", pattern = "*.png$", full.names = TRUE))
+             list.files("graphs", pattern = "*.png$", full.names = TRUE))
 file.remove(outputs)
 
 ## run my scripts
-source("code/style.R") # Not important, just make RPubs file beautiful
-source("HW6_part1.R")
-source("HW6_part2.R")
+source("code/HW6_part1.R")
+source("code/HW6_part2.R")
